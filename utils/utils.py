b3fd7aa4beee6838c7d3cb5f57ae5e9a09689108
f-salehi <farnood.salehi@epfl.ch>
2019-06-02 19:51:03 +0200

    Added length

--- utils/utils.py
+++ utils/utils.py
@@ -82,6 +82,11 @@ def pgd(inputs, net, epsilon=[1.], targets=None, step_size=0.04, num_steps=20, e
     
     return r_tot.cpu()
 
+
+TOTAL_BAR_LENGTH = 65.
+last_time = time.time()
+begin_time = last_time
+
 def progress_bar(current, total, msg=None):
     global last_time, begin_time
     if current == 0:
